409d5f8d3c368b9bb86a4dfb98b003271da45fcc
Guiille <guillebril@gmail.com>
2022-08-26 16:06:28 -0300

    code challenge

---
+++ src/pages/challengeOne.js
@@ -0,0 +1,14 @@
+/*
+1) Clone this repo on your local machinne.
+
+2) Use the data from accordion.json to create an accordion that simulates the look and feel of the
+Google search component that shows what People Also Ask.
+Try seaching "what is the best time to go to bed" on google to see how the component looks like.
+
+3) sort the list of accordion itmes by the length of the title.
+
+4) remove duplicated items from the accordion.
+
+Side notes: try to keep a SEO friendly approach. 
+
+*/
